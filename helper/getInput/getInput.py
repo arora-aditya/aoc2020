db8b7d354963b72819d1da6ad4b0c6caecc0a216
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2020-11-30 22:48:03 -0500

    update to create source files as well

--- helper/getInput/getInput.py
+++ helper/getInput/getInput.py
@@ -6,11 +6,13 @@ from requests import get
 
 YEAR = 2020
 
+
 def get_cookie():
     load_dotenv()
     return os.getenv("COOKIE")
 
-def download(url, file_name):
+
+def download(url: str, file_name: str):
     cookies = {
         "session": get_cookie()
     }
@@ -18,12 +20,26 @@ def download(url, file_name):
         response = get(url, cookies=cookies)
         file.write(response.content)
 
-def download_input_for_day(day):
+
+def create_source_files(day: int):
+    for i in range(2):
+        with open(f"./day{day}/part{i+1}.py", "w") as file:
+            file.write("")
+
+
+def download_input_for_day(day: int):
     url = f"https://adventofcode.com/{YEAR}/day/{day}/input"
     file_path = f"./day{day}/input.txt"
+    if os.path.exists(file_path):
+        return False
     download(url, file_path)
-    
+    return True
+
+
 def get_day_from_args():
     return int(sys.argv[1])
 
-download_input_for_day(get_day_from_args())
+
+day = get_day_from_args()
+if download_input_for_day(day):
+    create_source_files(day)
